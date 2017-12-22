bb93b39d5ebf82e6a546b1fbc4710645c68729fd
András Vidosits <vidosits.andras@gmail.com>
2017-12-22 17:31:00 +0100

    complex hozzáadása

--- hazik/Complex.py
+++ hazik/Complex.py
@@ -1,30 +1,13 @@
 import math
 
 
-class Complex(object):
-    _real = 0
-    _imaginary = 0
-    _magnitude = 0
-    _phase = 0
-
-    def real(self):
-        return self._real
-
-    def imaginary(self):
-        return self._imaginary
-
-    def magnitude(self):
-        return self._magnitude
-
-    def phase(self):
-        return self._phase * (180 / math.pi)
-
+class Complex:
     def __init__(self, real=0, imaginary=0):
         self.real = real
         self.imaginary = imaginary
 
     def __repr__(self):
-        return "{}+{}j\t Magnitude: {:0.2f} \t Phase: {:0.2f}°".format(self._real, self._imaginary, self._magnitude, self._phase * (180 / math.pi))
+        return ""
 
     def __add__(self, other):
         return Complex(self.real + other.real, self.imaginary + other.imaginary)
